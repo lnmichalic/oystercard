a7c967febb14ebea1b9ebd1b4e3f0206068ada05
Sam Pritchard <sampritchard@msn.com>
2017-05-16 12:07:39 +0100

    8 - created instance variable @in_journey and #touch_in #touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,18 +1,29 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey
+  alias_method :in_journey?, :in_journey
+
   MAXIMUM_BALANCE = 90
 
   def initialize()
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
-      raise "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + amount > MAXIMUM_BALANCE
-      @balance = @balance + amount
+    raise "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if @balance + amount > MAXIMUM_BALANCE
+    @balance += amount
   end
 
   def deduct(amount)
   	@balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,24 +1,61 @@
 require 'oystercard'
 
 describe Oystercard do
+	subject(:oystercard) { described_class.new }
+
 	it { is_expected.to respond_to(:top_up).with(1).argument }
 	it { is_expected.to respond_to(:deduct).with(1).argument }
-	
+	it { is_expected.to respond_to(:in_journey?) }
+	it { is_expected.to respond_to(:touch_in) }
+	it { is_expected.to respond_to(:touch_out) }
+
 	it "instance has default value of 0" do
-		expect(subject.balance).to eq(0)
+		expect(oystercard.balance).to eq(0)
 	end
 
+describe '#top_up' do
 	it "can top up the balance" do
-		expect{ subject.top_up 1}.to change{subject.balance}.by 1
+		expect{ oystercard.top_up 1}.to change{oystercard.balance}.by 1
 	end
-	
+end
+
 	it 'raises an error if the maximum balance is exceeded' do
   	maximum_balance = Oystercard::MAXIMUM_BALANCE
-  	subject.top_up maximum_balance
-  	expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+  	oystercard.top_up maximum_balance
+  	expect{ oystercard.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
 	end
-	
+
+describe '#deduct' do
 	it 'can deduct from balance' do
-	expect{ subject.deduct 1}.to change{subject.balance}.by -1
+	expect{ oystercard.deduct 1}.to change{oystercard.balance}.by -1
 	end
+end
+
+describe '#in_journey?' do
+	it 'returns false when oystercard initialized' do
+		expect(oystercard.in_journey?).to eq false
+	end
+	it 'returns true when oystercard is touched in' do
+		oystercard.touch_in
+		expect(oystercard.in_journey?).to eq true
+	end
+	it 'returns false when oystercard is touched out' do
+		oystercard.touch_in
+		oystercard.touch_out
+		expect(oystercard.in_journey?).to eq false
+	end
+end
+
+describe '#touch_in' do
+	it 'returns true' do
+		expect(oystercard.touch_in).to eq true
+	end
+end
+
+describe '#touch_out' do
+	it 'returns false' do
+		expect(oystercard.touch_out).to eq false
+	end
+end
+
 end
