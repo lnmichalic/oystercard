b146d30c1969e1cf8b3f140303efe5ad3034927a
Kynosaur <kye.bracey@gmail.com>
2017-05-18 16:43:40 +0100

    Update Oystercard to interact properly with Journey class, original functionality restored - remove some defunct tests from Oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,15 @@
+require_relative 'journey'
+
 class Oystercard
-  attr_reader :balance, :entry_station, :exit_station, :in_journey,:list_of_journeys, :journey
+  attr_reader :balance, :list_of_journeys, :journey
 
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
     @list_of_journeys = []
-    @journey = {}
+    @journey = Journey.new
   end
 
   def top_up(amount)
@@ -18,33 +19,29 @@ class Oystercard
 
   def touch_in(entry_station)
   	fail "Balance too low : Top up Please" if @balance < MINIMUM_BALANCE
-    # start_journey(entry_station)
-  	@entry_station = entry_station
-    @in_journey = true
-  end
-
-  def in_journey?
-  	!exit_station
+    start_journey(entry_station)
   end
 
   def touch_out(exit_station)
-    deduct(MINIMUM_BALANCE)
-    @entry_station
-    @exit_station = exit_station
+    @journey.update_exit_point(exit_station)
+    deduct(@journey.fare)
     save_journey_to_list
-    @exit_station
+    @journey = Journey.new
   end
 
   def save_journey_to_list
-    @journey = Journey.new(entry_station, exit_station)
     @list_of_journeys << @journey
   end
 
   private
 
-  # def start_journey(entry_station, exit_station)
-  #   journey = Journey.new(entry_station, exit_station)
-  # end
+  def start_journey(entry_station)
+    if @journey.entry_point
+      deduct(@journey.fare)
+      save_journey_to_list
+    end
+    @journey = Journey.new(entry_station, nil)
+  end
 
   def deduct(amount)
     @balance -= amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,10 +6,8 @@ describe Oystercard do
   let(:exit_station) { double :exit_station }
   let(:journey) { double :journey }
   it { is_expected.to respond_to(:top_up).with(1).argument }
-  it { is_expected.to respond_to(:in_journey?) }
   it { is_expected.to respond_to(:touch_in).with(1).argument }
   it { is_expected.to respond_to(:touch_out).with(1).argument }
-  it { is_expected.to respond_to(:entry_station) }
 
 
   it "instance has default value of 0" do
@@ -29,19 +27,9 @@ describe Oystercard do
   end
 
   describe '#touch_in' do
-    it 'Allows touch in when sufficient credit present' do
-      oystercard.top_up 90
-      expect(oystercard.touch_in(entry_station)).to eq true
-    end
 
     it 'Raises an error when balance below £1' do
       expect{oystercard.touch_in(entry_station)}.to raise_error "Balance too low : Top up Please"
-    end
-
-    it 'Shows us the entry_station last touched in at' do
-      oystercard.top_up 10
-      oystercard.touch_in(entry_station)
-      expect(oystercard.entry_station).to eq entry_station
     end
 
     it ' shows that a card has an empty list of journeys' do
