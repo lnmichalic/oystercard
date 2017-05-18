94dbb2ef08994308bae7290867566d4b9732d6d7
Lubos Michalic <lnmichalic@gmail.com>
2017-05-18 16:16:11 +0100

    Write tests for #fare and #in-progress methods, and write production code to pass tests

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,26 +1,31 @@
 class Journey
 
-	MINIMUM_FARE = 1
-	PENALTY_FARE = 6
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
 
-	attr_reader :entry_point, :exit_point
+  attr_reader :entry_point, :exit_point
 
-	def initialize(entry_point)
-		@entry_point = entry_point
-		@exit_point
-	end
+  def initialize(entry_point = nil, exit_point = nil)
+    @entry_point = entry_point
+    @exit_point = exit_point
 
-	def show_last_trip
-		self
-	end
+  end
 
-	def fare
-		return PENALTY_FARE if @entry_point == nil
-		MINIMUM_FARE
-	end
+  def show_last_trip
+    self
+  end
 
-	def in_progress?
-		!!@entry_point
-	end
+  def fare
+  return PENALTY_FARE if (@entry_point == nil || @exit_point == nil )
+  MINIMUM_FARE
+  end
 
+  def in_progress?
+    !!@entry_point && !@exit_point
+  end
+
+  def update_exit_point(station)
+    @exit_point = station
+
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,7 +4,7 @@ describe Journey do
   subject(:journey){ described_class.new("entry point")}
   let(:oystercard) {double("oystercard", touch_in: "entry station", touch_out: "exit station")}
   let(:oystercard2) {double("oystercard", touch_in: nil, touch_out: nil)}
-  let(:journey_with_no_touch_in) { described_class.new(nil) }
+  let(:journey_with_no_touch_in) { described_class.new(nil, 'Aldgate') }
 
   it 'is a type of Journey' do
   expect(journey).to be_a Journey
@@ -20,17 +20,18 @@ describe Journey do
 
   describe '#fare' do
     it 'calculates the fare' do
-      expect(journey.fare).to eq described_class::MINIMUM_FARE
+      expect(journey.fare).to eq described_class::PENALTY_FARE
     end
 
     it "charges the penalty fare if a passenger doesn't touch in" do
-      # oystercard2.touch_in(nil)
-      p @entry_point
-      # oystercard2.touch_out('Aldgate')
-      p @entry_point
-      p journey_with_no_touch_in.entry_point
       expect(journey_with_no_touch_in.fare).to eq described_class::PENALTY_FARE
     end
+
+    it "charges the minimum fare if the journey is complete" do
+      journey.update_exit_point('exit point')
+      expect(journey.fare).to eq described_class::MINIMUM_FARE
+    end
+
   end
 
   describe '#in_progress?' do
@@ -38,6 +39,12 @@ describe Journey do
       oystercard.touch_in("entry_station")
       expect(journey).to be_in_progress
     end
+
+    it 'returns false if the journey is completed' do
+      journey.update_exit_point('exit point')
+      expect(journey).not_to be_in_progress
+    end
+
   end
 
 end
